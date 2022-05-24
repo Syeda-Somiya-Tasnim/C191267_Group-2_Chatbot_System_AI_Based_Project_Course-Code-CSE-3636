83dddb605399cf4020906f4f1d10495c02651237
tasnimsamin <72883710+tasnimsamin@users.noreply.github.com>
2022-05-25 01:17:00 +0600

    Add files via upload

---
+++ Basic Build of chatbot/chatbot.py
@@ -0,0 +1,10 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+chatBot = ChatBot('ChatBot')
+
+trainer=ChatterBotCorpusTrainer(chatBot)
+
+trainer.train("chatterbot.corpus.english")
+
+print(chatBot.get_response("what is AI?"))
